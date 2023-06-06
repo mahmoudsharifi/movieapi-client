c23a962ce023f90c82a0112c1b61f730b84e1b3e
Mahmoud <mahmoudsharifi@hotmail.com>
2023-06-06 22:08:58 +0500

    props

--- src/components/MovieView/MovieView.js
+++ src/components/MovieView/MovieView.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
@@ -27,4 +28,20 @@ export const MovieView = ({ movie, onBackClick }) => {
       <button onClick={onBackClick}>Back</button>
     </div>
   )
+}
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }),
+    Description: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }),
+    Featured: PropTypes.string.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 }
